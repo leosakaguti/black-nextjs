9203ce72a7045af23bc7bc409ade204a98081436
Leonardo Sakaguti <sakaguti.leo@gmail.com>
2024-02-25 18:57:38 -0300

    Commit inicial

---
+++ src/pages/cart.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+export default function Products() {
+  return (
+    <>
+      <Head>
+        <title>Carrinho</title>
+        <meta name="description" content="Meu carrinho de compras" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Carrinho</h1>
+    </>
+  );
+}
---
+++ src/pages/products.tsx
@@ -0,0 +1,16 @@
+import { NextPage } from "next";
+import Head from "next/head";
+
+export default function Products() {
+  return (
+    <>
+      <Head>
+        <title>Nossos Produtos</title>
+        <meta name="description" content="Conheça nossos produtos" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Nossos Produtos</h1>
+    </>
+  );
+}
